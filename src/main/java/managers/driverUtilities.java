816f4dd341d91d0bd31f3320ae3615c51c8aa8be
haule2020 <huyhau2015@gmail.com>
2021-09-13 02:30:19 +0700

    haule

--- src/main/java/managers/driverUtilities.java
+++ src/main/java/managers/driverUtilities.java
@@ -2,6 +2,7 @@ package managers;
 
 import java.io.File;
 import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -13,7 +14,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 public class driverUtilities {
 
