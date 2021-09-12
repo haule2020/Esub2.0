44d7d16580783a673ef4d2bccf10f9fb8e84aac5
haule2020 <huyhau2015@gmail.com>
2021-09-13 02:41:48 +0700

    haule2

--- src/test/java/runner/CrmTest.java
+++ src/test/java/runner/CrmTest.java
@@ -9,8 +9,9 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(snippets = SnippetType.CAMELCASE,
 		strict = true,
-		features = "testCases/eClaim_Feature/Crm.feature", glue = "stepDefinitions"	
-		)
+		features = "testCases/eClaim_Feature/Crm.feature", glue = "stepDefinitions" ,plugin = { "json:target/cucumber-reports/Cucumber.json",
+"html:target/cucumber-reports/cucumber-pretty" })	
+		
 
 public class CrmTest  {
 
